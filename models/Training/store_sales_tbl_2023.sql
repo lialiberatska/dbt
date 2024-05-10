7713db9656b9049890594cc10f03f0cd251ed7f6
Yuliia Liberatska <lialiberatska@gmail.com>
2024-05-10 14:17:04 +0000

    source added

--- models/Training/store_sales_tbl_2023.sql
+++ models/Training/store_sales_tbl_2023.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 with store_sales_tbl_2023 as
 (
-select SS_STORE_SK, SS_QUANTITY, SS_SALES_PRICE, SS_NET_PROFIT from Prod.STORE_SALES_2023
+select SS_STORE_SK, SS_QUANTITY, SS_SALES_PRICE, SS_NET_PROFIT from {{ source('Snowflake', 'Store_Sales_2023')}}
 )
 select * from store_sales_tbl_2023
--- models/Training/store_tbl_2023.sql
+++ models/Training/store_tbl_2023.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 with store_tbl_2023 as
 (
-select S_STORE_SK, S_STORE_ID, S_STORE_NAME, S_COUNTRY from Prod.STORE_2023
+select S_STORE_SK, S_STORE_ID, S_STORE_NAME, S_COUNTRY from {{ source('Snowflake', 'Store_2023')}}
 )
 select * from store_tbl_2023
